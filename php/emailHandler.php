fb5d82cb44590462066fd0db2af5ca54a904efaf
EmmanuelAddi <emmanuelbett82108@gmail.com>
2023-06-30 16:06:45 +0300

    UI Modifictaions

--- php/emailHandler.php
+++ php/emailHandler.php
@@ -1,21 +1,37 @@
-<?php 
-// Send email via HTML
-$to      = 'nobody@example.com';
-$subject = isset($_REQUEST['subject']) ? strip_tags($_REQUEST['subject']) : 'the subject';
-$message = isset($_REQUEST['message']) ? strip_tags($_REQUEST['message']) : 'the message';
-$from = isset($_REQUEST['email']) ? strip_tags($_REQUEST['email']) . '<' .strip_tags($_REQUEST['email']) . '>' : '';
+<?php
 
+$to = 'emmanuelbett82108@gmail.com';
+$subject = isset($_POST['subject']) ? $_POST['subject'] : 'No subject';
+$message = isset($_POST['message']) ? $_POST['message'] : 'No message';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
 
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: ' . ( $from ) . '' . "\r\n";
-
-if( mail( $to, $subject, $message, $headers ) ){
-	die( json_encode(array(
-		'status' => 'Email send successfully!'
-	)));	
+// Validate email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die(json_encode(array(
+        'status' => 'Invalid email address'
+    )));
 }
 
-die( json_encode(array(
-	'status' => 'Unable to send Email. Try again later.'
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: ' . $email . "\r\n";
+
+// Validate subject and message
+if (empty($subject) || empty($message)) {
+    die(json_encode(array(
+        'status' => 'Subject and message are required'
+    )));
+}
+
+// Send email
+if (mail($to, $subject, $message, $headers)) {
+    die(json_encode(array(
+        'status' => 'Email sent successfully!'
+    )));
+}
+
+die(json_encode(array(
+    'status' => 'Unable to send email. Please try again later.'
 )));
+
+?>
